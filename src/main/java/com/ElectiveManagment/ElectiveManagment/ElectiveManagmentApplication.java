014b8f84c4c9930038d4e039e19a32345c25c054
Vishal <vishalaggarwal270@gmail.com>
2025-05-22 22:12:41 +0530

    Project SetUp

---
+++ src/main/java/com/ElectiveManagment/ElectiveManagment/ElectiveManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.ElectiveManagment.ElectiveManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectiveManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectiveManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ElectiveManagment/ElectiveManagment/ElectiveManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ElectiveManagment.ElectiveManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectiveManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
